0ca911a54610175683a910d4d0e65721997b2c59
Elven Decker Adama <sailing@together.net>
2020-12-01 15:49:18 -0500

    Initial attempts at recognizing intent

--- __init__.py
+++ __init__.py
@@ -1,14 +1,27 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+import re
 
 
 class StreamPlayer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('player.stream.intent')
-    def handle_player_stream(self, message):
-        self.speak_dialog('player.stream')
-
+    @intent_handler('player.stream.intent')
+    def handle_stream_wamc_intent(self, message):
+        self.speak_dialog("listen to this")
+        print('Message: "{}"'.format(message.data.get('utterance')))
+        mystring = message.data.get('utterance')
+        self.speak_dialog(mystring)
+        LOG.info(mystring)
+        
+        stations = [ 'wamc', 'weqx' ]
+        
+        if re.search(stations, mystring):
+        	pass
+        else:
+        	self.speak_dialog("Please enter that station in your Stream Player skill setup"
+        	
 
 def create_skill():
     return StreamPlayer()
